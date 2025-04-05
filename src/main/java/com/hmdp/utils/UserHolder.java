af6552cda7228f71d34decbd4945344767f35626
zhangchao <13722436010@163.com>
2025-04-05 21:12:32 +0800

    基于redis实现短信登录业务代码开发

--- src/main/java/com/hmdp/utils/UserHolder.java
+++ src/main/java/com/hmdp/utils/UserHolder.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+
 import com.hmdp.dto.UserDTO;
 
 public class UserHolder {
@@ -16,4 +17,18 @@ public class UserHolder {
     public static void removeUser(){
         tl.remove();
     }
+
+//    private static final ThreadLocal<User> tl = new ThreadLocal<>();
+//
+//    public static void saveUser(User user){
+//        tl.set(user);
+//    }
+//
+//    public static User getUser(){
+//        return tl.get();
+//    }
+//
+//    public static void removeUser(){
+//        tl.remove();
+//    }
 }
