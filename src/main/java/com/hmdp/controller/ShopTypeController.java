1a185b9f53f9ed9550259fd51ad2de717cc8ca83
zhangchao <13722436010@163.com>
2025-04-08 21:01:56 +0800

    店铺类型查询缓存业务代码开发

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -4,6 +4,7 @@ package com.hmdp.controller;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +22,17 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/shop-type")
+@Slf4j
 public class ShopTypeController {
     @Resource
     private IShopTypeService typeService;
 
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
+//        List<ShopType> typeList = typeService
+//                .query().orderByAsc("sort").list();
+        log.info("查询店铺类型列表");
+        List<ShopType> typeList = typeService.queryTypeList();
         return Result.ok(typeList);
     }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Shop;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +27,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public List<ShopType> queryTypeList() {
+        String key = RedisConstants.CACHE_SHOP_TYPE_KEY;
+
+        //1.从redis查询商铺缓存
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(key);
+        //2.判断是否存在
+        if (StrUtil.isNotBlank(shopTypeJson)) {
+            //3.存在 返回
+            List<ShopType> list = JSONUtil.toList(shopTypeJson, ShopType.class);
+            return list;
+        }
+        //4.不存在 查询数据库 tb_shop_type 表
+        List<ShopType> typeList = this.query().orderByAsc("sort").list();
+
+        //5.不存在 返回空列表
+        if (typeList.isEmpty()) {
+            return new ArrayList<>();
+        }
+        //6.存在 写入redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList));
+
+        // 7. 返回
+        return typeList;
+
+
+
+
+    }
 }
