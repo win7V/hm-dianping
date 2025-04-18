40d89395674f4f4537378123e41dd7a5fbb1fb46
zhangchao <13722436010@163.com>
2025-04-18 14:57:20 +0800

    乐观锁解决超卖问题代码开发

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -29,26 +29,26 @@ class HmDianPingApplicationTests {
     //创建500个线程
     private ExecutorService es = Executors.newFixedThreadPool(500);
 
-    @Test
-    void testIdWorker() throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(300);
-        Runnable task = () -> {
-            for (int i = 0; i < 100; i++) {
-                long id = redisIdWorker.nextId("order");
-                System.out.println("id = " + id);
-            }
-            latch.countDown();
-        };
-        long begin = System.currentTimeMillis();
-
-        for (int i = 0; i < 300; i++) {
-            es.submit(task);
-        }
-
-        latch.await();
-        long end = System.currentTimeMillis();
-        System.out.println("time = " + (end - begin));
-    }
+//    @Test
+//    void testIdWorker() throws InterruptedException {
+//        CountDownLatch latch = new CountDownLatch(300);
+//        Runnable task = () -> {
+//            for (int i = 0; i < 100; i++) {
+//                long id = redisIdWorker.nextId("order");
+//                System.out.println("id = " + id);
+//            }
+//            latch.countDown();
+//        };
+//        long begin = System.currentTimeMillis();
+//
+//        for (int i = 0; i < 300; i++) {
+//            es.submit(task);
+//        }
+//
+//        latch.await();
+//        long end = System.currentTimeMillis();
+//        System.out.println("time = " + (end - begin));
+//    }
 
 //    @Test
 //    void testSaveShop() throws InterruptedException {
