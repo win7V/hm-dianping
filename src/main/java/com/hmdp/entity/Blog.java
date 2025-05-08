7d3a2edc9eb407da69c5512b89abc1b59801f3cb
zhangchao <13722436010@163.com>
2025-05-08 21:09:02 +0800

    达人探店-发布查询探店笔记代码开发

--- src/main/java/com/hmdp/entity/Blog.java
+++ src/main/java/com/hmdp/entity/Blog.java
@@ -43,7 +43,7 @@ public class Blog implements Serializable {
     /**
      * 用户图标
      */
-    @TableField(exist = false)
+    @TableField(exist = false) // 当前字段不属于blog对应的表
     private String icon;
     /**
      * 用户姓名
